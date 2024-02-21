3c185ab7e04ed3b8b7863c6f42ae23bb0757cff3
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-21 21:16:15 +1300

    Windows support

--- build.rs
+++ build.rs
@@ -1,16 +1,36 @@
+use std::env;
 use std::path::PathBuf;
 
 pub fn main() {
-    let var = |name| {
-        std::env::var(name).expect(name)
+    let var = |name| env::var(name).expect(name);
+
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+
+    let (header, lib_dir) = match (env::var("BBL_USD_HEADER"), env::var("BBL_USD_LIB_DIR")) {
+        (Ok(header), Ok(lib_dir)) => (header, lib_dir),
+        _ => {
+            let dst = cmake::Config::new("bbl-usd")
+                .define("VULKAN_SDK", var("VULKAN_SDK"))
+                .define("BBL_LANGUAGES", "rust")
+                .build();
+
+            println!("cargo:rerun-if-changed=bbl-usd");
+
+            (
+                format!("{}/build/openusd-c.h", dst.display()),
+                format!("{}/build", dst.display()),
+            )
+        }
     };
 
-    let dst = cmake::Config::new("bbl-usd")
-        .define("VULKAN_SDK", var("VULKAN_SDK"))
-        .define("BBL_LANGUAGES", "rust")
-        .build();
+    bindgen::builder()
+        .header(header)
+        .generate()
+        .unwrap()
+        .write_to_file(out_path.join("openusd.rs"))
+        .unwrap();
 
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo:rustc-link-search=native={}", lib_dir);
     println!("cargo:rustc-link-lib=static=openusd-c");
     println!("cargo:rustc-link-lib=stdc++");
     println!("cargo:rustc-link-search=native={}", var("OPENUSD_LIB_DIR"));
@@ -75,29 +95,42 @@ pub fn main() {
         println!("cargo:rustc-link-lib=static={lib}");
     }
 
-    println!("cargo:rustc-link-search=native={}", var("TBB_LIB_DIR"));
-    println!("cargo:rustc-link-lib=static=tbb");
+    let building_for_windows = var("TARGET") == "x86_64-pc-windows-gnu";
 
-    println!("cargo:rustc-link-search=native={}", var("OPENSUBDIR_LIB_DIR"));
+    println!("cargo:rustc-link-search=native={}", var("TBB_LIB_DIR"));
+    println!(
+        "cargo:rustc-link-lib=static={}",
+        if building_for_windows {
+            "tbb12.dll"
+        } else {
+            "tbb"
+        }
+    );
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        var("OPENSUBDIR_LIB_DIR")
+    );
     println!("cargo:rustc-link-lib=static=osdCPU");
     println!("cargo:rustc-link-lib=static=osdGPU");
 
     println!("cargo:rustc-link-search=native={}", var("SHADERC_LIB_DIR"));
     println!("cargo:rustc-link-lib=static=shaderc_combined");
 
-    println!("cargo:rustc-link-lib=X11");
+    if building_for_windows {
+        println!("cargo:rustc-link-search=native={}/lib", var("VULKAN_SDK"));
 
-    println!("cargo:rustc-link-search=native={}", var("OPENGL_LIB_DIR"));
-    println!("cargo:rustc-link-lib=GLX");
+        println!(
+            "cargo:rustc-link-search=native={}",
+            var("MCFGTHREAD_LIB_DIR")
+        );
+        println!("cargo:rustc-link-lib=dylib=mcfgthread-1");
 
-    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+        println!("cargo:rustc-link-search=native={}", var("PTHREAD_LIB_DIR"));
+    } else {
+        println!("cargo:rustc-link-lib=X11");
 
-    bindgen::builder()
-        .header(out_path.join("build/openusd-c.h").to_str().unwrap())
-        .generate()
-        .unwrap()
-        .write_to_file(out_path.join("build/openusd.rs"))
-        .unwrap();
-
-    println!("cargo:rerun-if-changed=bbl-usd");
+        println!("cargo:rustc-link-search=native={}", var("OPENGL_LIB_DIR"));
+        println!("cargo:rustc-link-lib=GLX");
+    }
 }
--- src/ffi.rs
+++ src/ffi.rs
@@ -3,4 +3,4 @@
 #![allow(non_snake_case)]
 #![allow(unused)]
 
-include!(concat!(env!("OUT_DIR"), "/build/openusd.rs"));
+include!(concat!(env!("OUT_DIR"), "/openusd.rs"));
